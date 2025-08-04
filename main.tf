df586ae6b8cb879166baac8d8f6ad548e9069543
Maxwell Castro <maxwellsullivan@gmail.com>
2025-08-03 21:34:00 -0300

    check

--- main.tf
+++ main.tf
@@ -26,7 +26,7 @@ resource "aws_vpc_security_group_ingress_rule" "instance" {
 
 resource "aws_vpc_security_group_egress_rule" "instance" {
   security_group_id = aws_security_group.instance.id
-
+  
   cidr_ipv4   = "0.0.0.0/0"
   from_port   = 0
   ip_protocol = "tcp"
